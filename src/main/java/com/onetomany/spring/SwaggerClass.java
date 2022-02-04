f8abea8617be6f4d9ba4d8cd3a4390d4f1ece968
IndiraYusupova <indausupova@gmail.com>
2022-02-04 22:38:19 +0600

    SwaggerClass

--- src/main/java/com/onetomany/spring/SwaggerClass.java
+++ src/main/java/com/onetomany/spring/SwaggerClass.java
@@ -1,8 +1,10 @@
 package com.onetomany.spring;
 
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,6 +23,13 @@ public class SwaggerClass {
                     .paths(PathSelectors.any())
                     .build();
 
+        }
+        private ApiInfo apiInfo() {
+            return new ApiInfoBuilder()
+                    .title ("Руководство по API проекта на основе Swagger")
+                    .description ("API компонента информационной службы")
+                    .version("1.0")
+                    .build();
         }
 
     }
--- src/main/java/com/onetomany/spring/controller/OrderController.java
+++ src/main/java/com/onetomany/spring/controller/OrderController.java
@@ -6,6 +6,8 @@ import com.onetomany.spring.Dto.OrderResponse;
 import com.onetomany.spring.entity.Customer;
 import com.onetomany.spring.repository.CustomerRepository;
 import com.onetomany.spring.repository.ProductRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Api(description = "Controller to illustrate operation Swagger")
 public class OrderController {
     @Autowired
     private CustomerRepository customerRepository;
@@ -22,14 +25,18 @@ public class OrderController {
     private ProductRepository productRepository;
 
     @PostMapping("/placeOrder")
+    @ApiOperation("save request")
+
     public Customer placeOrder(@RequestBody OrderRequest request){
         return customerRepository.save(request.getCustomer());
     }
     @GetMapping("/findAllOrders")
+    @ApiOperation("Getting a list of orders")
     public List<Customer> findAllOrders(){
         return customerRepository.findAll();
     }
     @GetMapping("/getInfo")
+    @ApiOperation("Getting complete information")
     public List<OrderResponse> getJoinInformation() {
         return customerRepository.getJoinInformation();
     }
